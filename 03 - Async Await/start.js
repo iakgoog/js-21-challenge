f47ac220f7ea62332f0e350a5c46c12594cd6ccf
Sutthinart Khunvadhana <iakgoog@gmail.com>
2021-04-13 12:00:08 +0700

    finish 03 - Async Await

--- 03 - Async Await/start.js
+++ 03 - Async Await/start.js
@@ -1,8 +1,67 @@
 (() => {
-  // เริ่มเขียนโค้ด
+  // function simulateAsyncAPI(text, timeout) {
+  //   setTimeout(() => console.log(text), timeout);
+  // }
 
-  // 1. How Asynchronous code works in JavaScript
-  // 2. Callback
-  // 3. Promise
-  // 4. Async/Await
+  // simulateAsyncAPI('A', 1000);
+  // simulateAsyncAPI('B', 500);
+  // simulateAsyncAPI('C', 100);
+
+  // function simulateAyncAPI(text, timeout, callback) {
+  //   setTimeout(() => {
+  //     console.log(text);
+  //     if (callback) {
+  //       callback();
+  //     }
+  //   }, timeout);
+  // }
+
+  // simulateAyncAPI('A', 1000, () => {
+  //   simulateAyncAPI('B', 500, () => {
+  //     simulateAyncAPI('C', 100);
+  //   });
+  // });
+
+  // function simulatAsyncAPI(text, timeout) {
+  //   return new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       if (text === 'B') return reject('B got rejected');
+  //       console.log(text);
+  //       resolve();
+  //     }, timeout);
+  //   });
+  // }
+
+  // simulatAsyncAPI('A', 1000)
+  //   .then(() => {
+  //     return simulatAsyncAPI('B', 500);
+  //   })
+  //   .then(() => {
+  //     return simulatAsyncAPI('C', 100);
+  //   })
+  //   .catch((error) => {
+  //     console.error(error);
+  //   });
+
+  function simulatAsyncAPI(text, timeout) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        // if (text === 'B') return reject('B got rejected');
+        console.log(text);
+        resolve();
+      }, timeout);
+    });
+  }
+
+  async function run() {
+    try {
+      await simulatAsyncAPI('A', 1000);
+      await simulatAsyncAPI('B', 500);
+      await simulatAsyncAPI('C', 100);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  run();
 })();
