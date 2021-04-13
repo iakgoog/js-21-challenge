2ee52e29103ba61ef446573d59a3a2f46d192b65
Sutthinart Khunvadhana <iakgoog@gmail.com>
2021-04-13 11:13:10 +0700

    finish 02 - Countdown Timer

--- 02 - Countdown Timer/start.js
+++ 02 - Countdown Timer/start.js
@@ -1,4 +1,28 @@
 (() => {
-  // เริ่มเขียนโค้ด
+  const SECOND = 1000;
+  const MINUTE = SECOND * 60;
+  const HOUR = MINUTE * 60;
+  const DAY = HOUR * 24;
 
+  function setElementInnerText(id, text) {
+    const element = document.getElementById(id);
+    element.innerText = text;
+  }
+
+  function countDown() {
+    const now = new Date().getTime();
+    const newYear = new Date('December 31, 2021 23:59:59').getTime();
+    const unixTimeLeft = newYear - now;
+
+    setElementInnerText('days', Math.floor(unixTimeLeft / DAY))
+    setElementInnerText('hours', Math.floor(unixTimeLeft % DAY / HOUR));
+    setElementInnerText('minutes', Math.floor(unixTimeLeft % DAY % HOUR / MINUTE))
+    setElementInnerText('seconds', Math.floor(unixTimeLeft % DAY % HOUR % MINUTE / SECOND));
+  }
+
+  function run() {
+    setInterval(countDown, SECOND);
+  }
+
+  run();
 })();
