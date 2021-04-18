a44128d4dd98363711465f0dd068bf339f701f76
Sutthinart Khunvadhana <iakgoog@gmail.com>
2021-04-18 07:32:39 +0700

    finish 07 - Konami Code

--- 07 - Konami Code/start.js
+++ 07 - Konami Code/start.js
@@ -1,4 +1,19 @@
 (() => {
-  // เริ่มเขียนโค้ด
+  // startSnowing();
+  const konamiPattern = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+  let patternHit = 0;
 
+  function konamiPatternHandler({ keyCode }) {
+    keyCode === konamiPattern[patternHit] ? patternHit++ : patternHit = 0;
+    if (patternHit === konamiPattern.length) {
+      patternHit = 0;
+      startSnowing();
+    }
+  }
+
+  function run() {
+    document.addEventListener('keyup', konamiPatternHandler);
+  }
+
+  run();
 })();
