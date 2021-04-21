fbc662a243d130d45ceee114463776a61f2fa696
Sutthinart Khunvadhana <iakgoog@gmail.com>
2021-04-21 07:26:32 +0700

    finish 08 - What is this

--- 08 - What is this/start.js
+++ 08 - What is this/start.js
@@ -1,10 +1,45 @@
 (() => {
-  // เริ่มเขียนโค้ด
+  // function printName() {
+  //   console.log(this);
+  //   console.log(`My name is ${this.name}`);
+  // }
 
-  // 1. Lexical scope & Dynamic scope
-  // 2. How to know what is "this"?
-  //// 2.1 Invoker object
-  //// 2.2 Global object (window, global)
-  //// 2.3 Constructor function
-  // 3. call(), apply(), and bind()
+  // // printName();
+  // const sutthinart = { name: 'Sutthinart', printName }
+  // const jane = { name: 'Jane', printName }
+
+  // sutthinart.printName();
+  // jane.printName();
+
+  // name = 'Global';
+  // printName();
+
+  // function Person(name) {
+  //   this.name = name;
+  //   this.printName = printName;
+  // }
+
+  // const sutthinart = new Person('Sutthinart');
+
+  // sutthinart.printName();
+
+  function printName(nationality, city) {
+    console.log(this);
+    console.log(`My name is ${this.name}, I'm ${nationality} and am living in ${city}`);
+  }
+
+  function Person(name, nationality, city) {
+    this.name = name;
+    this.nationality = nationality;
+    this.city = city;
+
+    printName(this.nationality, this.city);
+    printName.call(this, this.nationality, this.city);
+    printName.apply(this, [this.nationality, this.city]);
+
+    const printSutthinart = printName.bind(this);
+    printSutthinart(this.nationality, this.city);
+  }
+
+  const sutthinart = new Person('Sutthinart', 'Thai', 'Chiangmai')
 })();
