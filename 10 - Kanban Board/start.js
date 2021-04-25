07be2c2e0a81b508bae1cb7ec112018e533e115d
Sutthinart Khunvadhana <iakgoog@gmail.com>
2021-04-25 07:34:49 +0700

    finish 10 - Kanban Board

--- 10 - Kanban Board/start.js
+++ 10 - Kanban Board/start.js
@@ -1,3 +1,39 @@
 (() => {
+  let draggignElem;
 
+  function onDragStart() {
+    draggingElem = this;
+    console.log(this);
+  }
+
+  function onDrop() {
+    console.log('Drop');
+    this.append(draggingElem);
+    draggignElem = null;
+  }
+
+  function onDragOver(event) {
+    event.preventDefault();
+  }
+
+  function onDragEnter(event) {
+    event.preventDefault();
+  }
+
+  function run() {
+    const taskElems = Array.from(document.querySelectorAll('.task'));
+    const dropZoneElems = Array.from(document.querySelectorAll('.drop-zone'));
+
+    taskElems.forEach((taskElem) => {
+      taskElem.addEventListener('dragstart', onDragStart);
+    });
+
+    dropZoneElems.forEach((dropZoneElems) => {
+      dropZoneElems.addEventListener('drop', onDrop);
+      dropZoneElems.addEventListener('dragover', onDragOver);
+      dropZoneElems.addEventListener('dragenter', onDragEnter);
+    })
+  }
+
+  run();
 })();
